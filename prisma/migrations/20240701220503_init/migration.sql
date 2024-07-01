bffe8212401c05142cf256c4226eeb792449eb6b
userName194 <willowio477@gmail.com>
2024-07-02 01:05:38 +0300

    fix schema

---
+++ prisma/migrations/20240701220503_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "VerificationCode" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationCode_pkey" PRIMARY KEY ("id")
+);
